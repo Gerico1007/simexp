38ae170bcebaf58ae5709bc905aeecc91413adda
Gerico1007 <gerico@jgwill.com>
2025-10-27 18:13:52 -0400

    chore: Bump version to 0.3.11 for clipboard fix release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='simexp',
-    version = "0.3.10",
+    version = "0.3.11",
     packages=find_packages(
         include=["simexp", "test-*.py"], exclude=["test*log", "*test*csv", "*test*png"]
     ),
