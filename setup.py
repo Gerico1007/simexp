33c3fd3637c1e7be0be9fcbd987ba4c5ef40dfd3
Gerico1007 <gerico@jgwill.com>
2025-11-17 17:04:39 -0500

    Bump version to 0.4.2 - Remove collab remove from help text

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='simexp',
-    version = "0.3.13",
+    version = "0.4.2",
     packages=find_packages(
         include=["simexp", "test-*.py"], exclude=["test*log", "*test*csv", "*test*png"]
     ),
